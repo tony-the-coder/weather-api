84bebad8915e64a45e56e88e601ba28858e47409
Tony <tonythecoder@outlook.com>
2022-12-20 10:19:02 -0500

    Adding basic functionality

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const { write } = require("fs");
-
 const app = express()
-
 const https = require('https');
+const bodyParser = require("body-parser")
 
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=3970d7d474b0f13604a947fb02f9809e&units=imperial"
+    res.sendFile(__dirname + "/index.html")
+
+
+
+});
+
+app.post("/", function (req, res) {
+    // console.log(req.body.cityName)
+    const query = req.body.cityName
+    const apiKey = "3970d7d474b0f13604a947fb02f9809e"
+    const units = "imperial"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units
     https.get(url, function (response) {
         console.log(response.statusCode)
 
@@ -20,15 +30,13 @@ app.get("/", function (req, res) {
             const icon = weatherData.weather[0].icon
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write('<h1>The weather is curretnly ' + description + '.</h1>')
-            res.write('<h1>The Temperature in Long is ' + temp + ' degrees fahrenheit</h1>')
+            res.write('<h1>The Temperature in ' + query + ' is ' + temp + ' degrees fahrenheit</h1>')
             res.write("<img  src=" + imageURL + ">")
             res.send();
 
         })
+
     })
-
-
-    // res.send("Server is up and running")
 })
 
 
